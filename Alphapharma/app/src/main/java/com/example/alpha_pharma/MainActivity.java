cf93e8c26c5dd7bf882c4aea46948b1b0617a12f
harsh patel <harsh0404>
2020-11-23 20:03:01 -0500

    commited

---
+++ Alphapharma/app/src/main/java/com/example/alpha_pharma/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.alpha_pharma;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
